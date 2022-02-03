50e7a9382d7c254e4cb9a6725027921994147a84
sumbel <s.enikeeva1@mail.ru>
2022-02-03 18:01:23 +0300

    simple unchecked train func

--- denseCL.py
+++ denseCL.py
@@ -36,11 +36,7 @@ class DenseCL(nn.Module):
 
     @torch.no_grad()
     def update_queues(self, k_g, k_d):
-        # batch_size = k_g.shape[0]
-        # self.queue_g[self.ptr: self.ptr + batch_size] = k_g
-        # self.queue_d[self.ptr: self.ptr + batch_size] = k_d
-        # self.ptr = (self.ptr + batch_size) % self.dict_size
-
+        # global average pooling
         k_d = nn.functional.normalize(k_d.mean(dim=2), dim=1)
         batch_size = k_g.shape[0]
 
@@ -52,6 +48,7 @@ class DenseCL(nn.Module):
 
         self.ptr[0] = ptr
 
+    # TODO: add shuffle
     def forward(self, in_q, in_k):
 
         bs = in_k.size(0)
@@ -69,86 +66,17 @@ class DenseCL(nn.Module):
             match_idx = cosine.argmax(dim=1)
             d_q = d_q.gather(2, match_idx.unsqueeze(1).expand(-1, self.dim, -1))
 
-        input_pos_g = torch.einsum('bd,bd->b', [g_q, g_k]).view(bs, 1)
-        input_neg_g = torch.einsum('bd,dq->bq', [g_q, self.queue_g.clone().detach()])
-        input_g = torch.concat((input_pos_g, input_neg_g), dim=1) / self.tau
-        labels_g = torch.zeros(input_g.size(0), dtype=torch.long)
+        output_pos_g = torch.einsum('bd,bd->b', [g_q, g_k]).view(bs, 1)
+        output_neg_g = torch.einsum('bd,dq->bq', [g_q, self.queue_g.clone().detach()])
+        output_g = torch.concat((output_pos_g, output_neg_g), dim=1) / self.tau
+        target_g = torch.zeros(output_g.size(0), dtype=torch.long)
+        # .cuda()
 
-        input_pos_d = torch.einsum('bdz,bdz->bz', [d_k, d_q]).view(bs, 1, d_q.size(-1))
-        input_neg_d = torch.einsum('bdz,dq->bqz', [d_q, self.queue_d.clone().detach()])
-        input_d = torch.concat((input_pos_d, input_neg_d), dim=1) / self.tau
-        labels_d = torch.zeros((input_d.size(0), input_d.size(-1)), dtype=torch.long)
+        output_pos_d = torch.einsum('bdz,bdz->bz', [d_k, d_q]).view(bs, 1, d_q.size(-1))
+        output_neg_d = torch.einsum('bdz,dq->bqz', [d_q, self.queue_d.clone().detach()])
+        output_d = torch.concat((output_pos_d, output_neg_d), dim=1) / self.tau
+        target_d = torch.zeros((output_d.size(0), output_d.size(-1)), dtype=torch.long)
+        # .cuda()
 
         self.update_queues(g_k, d_k)
-        return input_g, labels_g, input_d, labels_d
-
-    # def forward(self, im_q, im_k):
-    #     """
-    #     Input:
-    #         im_q: a batch of query images
-    #         im_k: a batch of key images
-    #     Output:
-    #         logits, targets
-    #     """
-    #
-    #     # compute query features
-    #     q, dense_q, feat_q = self.encoder_q(im_q)  # queries: NxC
-    #     q = nn.functional.normalize(q, dim=1)
-    #     n, c, h, w = feat_q.size()
-    #     dim_dense = dense_q.size(1)
-    #     dense_q, feat_q = dense_q.view(n, dim_dense, -1), feat_q.view(n, c, -1)
-    #     dense_q = nn.functional.normalize(dense_q, dim=1)
-    #
-    #     # compute key features
-    #     with torch.no_grad():  # no gradient to keys
-    #         self.momentum_update_key_encoders()  # update the key encoder
-    #
-    #         # # shuffle for making use of BN
-    #         # im_k, idx_unshuffle = self._batch_shuffle_ddp(im_k)
-    #
-    #         k, dense_k, feat_k = self.encoder_k(im_k)  # keys: NxC
-    #         k = nn.functional.normalize(k, dim=1)
-    #         dense_k, feat_k = dense_k.view(n, dim_dense, -1), feat_k.view(n, c, -1)
-    #         dense_k_norm = nn.functional.normalize(dense_k, dim=1)
-    #
-    #         # # undo shuffle
-    #         # k, feat_k, dense_k_norm = self._batch_unshuffle_ddp(
-    #         #         k, feat_k, dense_k_norm, idx_unshuffle)
-    #
-    #         ## match
-    #         feat_q_norm = nn.functional.normalize(feat_q, dim=1)
-    #         feat_k_norm = nn.functional.normalize(feat_k, dim=1)
-    #         cosine = torch.einsum('nca,ncb->nab', feat_q_norm, feat_k_norm)
-    #         pos_idx = cosine.argmax(dim=-1)
-    #         dense_k_norm = dense_k_norm.gather(2, pos_idx.unsqueeze(1).expand(-1, dim_dense, -1))
-    #
-    #     # compute logits
-    #     # Einstein sum is more intuitive
-    #     # positive logits: Nx1
-    #     l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
-    #     # negative logits: NxK
-    #     l_neg = torch.einsum('nc,ck->nk', [q, self.queue_g.clone().detach()])
-    #
-    #     # logits: Nx(1+K)
-    #     logits = torch.cat([l_pos, l_neg], dim=1)
-    #
-    #     # apply temperature
-    #     logits /= self.tau
-    #
-    #     # labels: positive key indicators
-    #     labels = torch.zeros(logits.shape[0], dtype=torch.long)
-    #
-    #
-    #     ## densecl logits
-    #     d_pos = torch.einsum('ncm,ncm->nm', dense_q, dense_k_norm).unsqueeze(1)
-    #     d_neg = torch.einsum('ncm,ck->nkm', dense_q, self.queue_d.clone().detach())
-    #     logits_dense = torch.cat([d_pos, d_neg], dim=1)
-    #     logits_dense = logits_dense / self.tau
-    #     labels_dense = torch.zeros((n, h*w), dtype=torch.long)
-    #
-    #     # dequeue and enqueue
-    #     self.update_queues(k, dense_k)
-    #
-    #     return logits, labels, logits_dense, labels_dense
-
-
+        return output_g, target_g, output_d, target_d
--- denseCL_main.py
+++ denseCL_main.py
@@ -1,46 +1,63 @@
-import torch
-import torchvision
-
-from data_service import DataAugmentation
-from backbone import BackBone, ResNet, Bottleneck
 import matplotlib.pyplot as plt
 import numpy as np
-import torch.optim as optim
+import torch
 import torch.nn as nn
+import torchvision
 
-
-# functions to show an image
+from backbone import BackBone
+from data_service import DataAugmentation
 from denseCL import DenseCL
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
 
-def train(train_loader, model):
+def train(train_loader, model, criterion, optimizer, **kwargs):
+    lmbd = kwargs['lmbd']
     model.train()
     for i, (images, _) in enumerate(train_loader):
-        logits, labels, logits_dense, labels_dense = model(images[0], images[1])
-        k = 1
+        output_g, target_g, output_d, target_d = model(images[0], images[1])
+        loss_g = criterion(output_g, target_g)
+        loss_d = criterion(output_d, target_d)
+        loss = lmbd * loss_g + (1 - lmbd) * loss_d
+
+        loss.backward()
+        optimizer.step()
 
 
-if __name__ == "__main__":
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    momentum = 0.9
+    weight_decay = 1e-4
+    lr = 0.3
+    batch_size = 4  # 256
+    start_epoch = 0
+    epochs = 3  # 800
+    lmbd = 0.5
+
     dataset_path = "~/fiftyone/coco-2017/validation/"
     train_set = torchvision.datasets.ImageFolder(root=dataset_path, transform=DataAugmentation())
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True)
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
     backbone_q = BackBone()
     backbone_k = BackBone()
 
     model = DenseCL(backbone_q, backbone_k)
+    print(model)
 
-    train(train_loader, model)
+    criterion = nn.CrossEntropyLoss()
+    # .cuda(args.gpu)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+    for epoch in range(start_epoch, epochs):
+        train(train_loader, model, criterion, optimizer, lmbd=lmbd)
 
 
-
-
+if __name__ == "__main__":
+    main()
